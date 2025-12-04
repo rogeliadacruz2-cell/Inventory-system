aa17a74a953b88677b3ef5b9ebd4229aca6553f1
zeia <rogeliadacruz2@gmail.com>
2025-12-05 01:01:03 +0700

    First commit - Inventory System

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Konfigurasi Firebase (Opsional - jika ingin pakai Firebase)
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+console.log("Firebase config loaded (disabled for now)");
